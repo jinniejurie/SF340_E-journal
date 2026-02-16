6769a6b81117cad035e62c753f0e46d816408f71
jinniejurie <rattananname@email.com>
2026-02-16 17:16:23 +0700

    feat (App): Add Account route and update package-lock.json

--- e-journal/src/App.jsx
+++ e-journal/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
+import Account from './pages/Account.jsx'
 import Calendar from './pages/Calendar.jsx'
 import ToDoList from './pages/ToDoList.jsx'
 import Note from './pages/Note.jsx'
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/account" element={<Account />} />
       <Route path="/calendar" element={<Calendar />} />
       <Route path="/calendar/toDoList" element={<ToDoList />} />
       <Route path="/calendar/note" element={<Note />} />
